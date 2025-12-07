5fb8604276c50aa1fafc759e2acfc2f246b628b4
foramthakker26 <foramthakker.120@gmail.com>
2025-12-07 21:25:37 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // MUST be at the top
+
+const connectDB = async () => {
+  try {
+    console.log("Mongo URI:", process.env.MONGO_URI); // Debug line
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
