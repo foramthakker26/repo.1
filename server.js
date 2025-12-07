9a4af99c11757615b36c469d672ce02c64cd94b6
foramthakker26 <foramthakker.120@gmail.com>
2025-12-07 18:57:24 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
